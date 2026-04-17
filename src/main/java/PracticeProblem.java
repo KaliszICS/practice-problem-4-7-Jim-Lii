d084d2a903ec074f8e10b186458bc9ec891e4fe0
Jim-Lii <343347795@tdsb.ca>
2026-04-17 02:20:02 +0000

    vhyubhbjhnjfsjk

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,39 @@
+/*
+File Name: Random module
+Author: Jim Li
+Date Created: Apr. 16, 2026
+Date Last Modified: Apr. 16, 2026
+ */
+
+import java.util.Random;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	}
+
+	public static int randomNumber(int num1, int num2){
+		Random random = new Random();
+		int start = Math.min(num1, num2);
+		int stop = Math.max(num1, num2);
+		return random.nextInt(stop - start + 1) + start;
+	}
+
+	public static char randomChar(String str){
+		Random random = new Random();
+		int ranNum = random.nextInt(str.length());
+		return str.charAt(ranNum);
+	}
+
+	public static int randomEvenNumber(int num1, int num2){
+		Random random = new Random();
+		int start = Math.min(num1, num2);
+		int stop = Math.max(num1, num2);
+		int num = 1;
+		while (num % 2 != 0){
+			num = random.nextInt(stop - start + 2) + start;
+		}
+		return num;
 	}
 
 }
